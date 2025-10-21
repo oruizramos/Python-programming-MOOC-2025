c1a7f578f9a0727626afe4cd037d977a5d42bc96
oruizramos <70069043+oruizramos@users.noreply.github.com>
2025-10-21 22:19:40 +0200

    Changed email to verified address for commits; Added an extra exercise solution for part 2.4

--- Part 2/2.4.py
+++ Part 2/2.4.py
@@ -59,3 +59,18 @@ while True:
 
 print("Now!")
 
+"""
+# Repeat password
+"""
+
+password = input("Password: ")
+
+while True:
+    repeat = input("Repeat password: ")
+    if repeat == password:
+        break
+    print("They do not match!")
+
+print("User account created!")
+
+
